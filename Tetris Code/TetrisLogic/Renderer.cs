8d7cad85e4d07c5835d784149150728475372238
Malgorzata Stepien <mrowka970@gmail.com>
2019-11-24 15:19:53 +0100

    Add Renderer class and main methods

---
+++ Tetris Code/TetrisLogic/Renderer.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
+
+namespace TetrisLogic
+{
+    /// <summary>
+    /// Game handler
+    /// </summary>
+    public static class Renderer
+    {
+        /// <summary>
+        /// Mouse position
+        /// </summary>
+        private static Point mousePos;
+
+        /// <summary>
+        /// Play button
+        /// </summary>
+        private static Button buttonPlay;
+
+        /// <summary>
+        /// Pause button
+        /// </summary>
+        private static Button buttonPause;
+
+        /// <summary>
+        /// Exit button
+        /// </summary>
+        private static Button buttonExit;
+
+        /// <summary>
+        /// Menu button
+        /// </summary>
+        private static Button buttonScores;
+
+        /// <summary>
+        /// Restart button
+        /// </summary>
+        private static Button buttonRestart;
+
+        /// <summary>
+        /// dashboard
+        /// </summary>
+        private static Panel panelGameOver;
+
+        /// <summary>
+        /// Pause panel
+        /// </summary>
+        private static Panel panelPause;
+
+        /// <summary>
+        /// Graphics size
+        /// </summary>
+        public static Size GraphicsSize { get; set; }
+
+        /// <summary>
+        /// Score
+        /// </summary>
+        public static int Score { get; set; }
+
+        /// <summary>
+        /// Score visibility
+        /// </summary>
+        public static bool ScoresVisible { get; set; }
+
+        /// <summary>
+        /// Pause visibility
+        /// </summary>
+        public static bool PauseVisible { get; set; }
+
+        /// <summary>
+        /// Scores
+        /// </summary>
+        public static List<KeyValuePair<string, int>> Scores { get; set; }
+
+        /// <summary>
+        /// Quit the game
+        /// </summary>
+        private static void Exit()
+        {
+            SaveScores();
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Pause
+        /// </summary>
+        public static void Pause()
+        {
+            if (Piece.Speed != 0)
+            {
+                Piece.Speed = 0;
+                PauseVisible = true;
+                buttonPause.Text = "Go on";
+            }
+            else
+            {
+                Piece.Speed = 0.015f;
+                PauseVisible = false;
+                buttonPause.Text = "Pause";
+            }
+        }
+
+        /// <summary>
+        /// Text drawing
+        /// </summary>
+        /// <param name="refGraphics">Graphics</param>
+        /// <param name="parText">Text</param>
+        /// <param name="parX">X position</param>
+        /// <param name="parY">Y position</param>
+        /// <param name="parW">Width</param>
+        /// <param name="parH">Height</param>
+        /// <param name="parS">Font size</param>
+        /// <param name="parC">Center or not</param>
+        /// <param name="parNeg">Negtive color or not</param>
+        public static void DrawString(Graphics refGraphics, string parText, float parX, float parY, float parW,
+         float parH, float parS, bool parC, bool parNeg)
+        {
+            refGraphics.DrawString(parText, new Font(FontFamily.GenericSansSerif, parW / parS / 2),
+             parNeg ? Brushes.White : Brushes.Black, new RectangleF(parX, parY, parW, parH),
+             new StringFormat
+             {
+                 Alignment = parC ? StringAlignment.Center : StringAlignment.Near,
+                 LineAlignment = StringAlignment.Center
+             });
+        }
+
+        /// <summary>
+        /// Get mouse position
+        /// </summary>
+        /// <param name="parPos">Mouse position</param>
+        public static void MousePos(Point parPos)
+        {
+            mousePos = parPos;
+        }
+
+        /// <summary>
+        /// Load highscores
+        /// </summary>
+        public static void LoadScores()
+        {
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Tetris/scores.score"))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                using (FileStream fs = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Tetris/scores.score", FileMode.OpenOrCreate))
+                {
+                    Scores = (List<KeyValuePair<string, int>>)formatter.Deserialize(fs);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Save highscores
+        /// </summary>
+        public static void SaveScores()
+        {
+            try
+            {
+                if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Tetris/"))
+                    Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Tetris/");
+                BinaryFormatter formatter = new BinaryFormatter();
+                using (FileStream fs = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Tetris/scores.score", FileMode.OpenOrCreate))
+                {
+                    formatter.Serialize(fs, Scores);
+                }
+            }
+            catch { };
+        }
+    }
+}
