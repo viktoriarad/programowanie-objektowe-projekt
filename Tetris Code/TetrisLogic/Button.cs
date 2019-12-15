4aa905e8909ceb1728204d8e3d7c4cbc345260da
Malgorzata Stepien <mrowka970@gmail.com>
2019-12-15 20:36:49 +0100

    Add Panel and Button classes

---
+++ Tetris Code/TetrisLogic/Button.cs
@@ -0,0 +1,103 @@
+﻿using System.Drawing;
+
+namespace TetrisLogic
+{
+    /// <summary>
+    /// Buttton
+    /// </summary>
+    public class Button
+    {
+        /// <summary>
+        /// Click function
+        /// </summary>
+        private readonly dСauseClick onClick;
+
+        /// <summary>
+        /// Text font size
+        /// </summary>
+        private const float FONT_SIZE = 20.0f;
+
+        /// <summary>
+        /// Button text
+        /// </summary>
+        public string Text;
+
+        /// <summary>
+        /// Delegate for click function
+        /// </summary>
+        public delegate void dСauseClick();
+
+        /// <summary>
+        /// Button position
+        /// </summary>
+        public PointF Posistion { set; get; }
+
+        /// <summary>
+        /// Buttion size
+        /// </summary>
+        public SizeF Size { set; get; }
+
+        /// <summary>
+        /// Button class constructor
+        /// </summary>
+        /// <param name="parText">Button text</param>
+        /// <param name="parOnClick">Click function</param>
+        public Button(string parText, dСauseClick parOnClick)
+        {
+            onClick = parOnClick;
+            Text = parText;
+        }
+
+        /// <summary>
+        /// Setting the position and size of the button
+        /// </summary>
+        /// <param name="parPos">Button position</param>
+        /// <param name="parSize">Buttion size</param>
+        public void SetBounds(PointF parPos, SizeF parSize)
+        {
+            Posistion = parPos;
+            Size = parSize;
+        }
+
+        /// <summary>
+        /// Click function call
+        /// </summary>
+        /// <param name="parMousePos">Mouse position</param>
+        public void Click(Point parMousePos)
+        {
+            if (MouseOn(parMousePos))
+                onClick();
+        }
+
+        /// <summary>
+        /// Button update function
+        /// </summary>
+        /// <param name="refGraphics">Graphics</param>
+        /// <param name="parMousePos">Mouse position</param>
+        /// <param name="parGraphicsSize">Graphics size</param>
+        public void Update(Graphics refGraphics, Point parMousePos, SizeF parGraphicsSize)
+        {
+            float w = 1 + (MouseOn(parMousePos) ? 4 : 2) * parGraphicsSize.Width / 800.0f;
+            refGraphics.DrawRectangle(new Pen(Color.White, w), Posistion.X, Posistion.Y, Size.Width, Size.Height);
+            StringFormat format = new StringFormat
+            {
+                LineAlignment = StringAlignment.Center,
+                Alignment = StringAlignment.Center
+            };
+            refGraphics.DrawString(Text, new Font("Times New Roman", parGraphicsSize.Width / (FONT_SIZE - w) / 2), Brushes.White,
+            new RectangleF(Posistion.X, Posistion.Y, Size.Width, Size.Height), format);
+        }
+
+        /// <summary>
+        /// Mouse hover function
+        /// </summary>
+        /// <param name="parMousePos"></param>
+        /// <returns></returns>
+        public bool MouseOn(Point parMousePos)
+        {
+            return parMousePos.X > Posistion.X && parMousePos.Y > Posistion.Y &&
+             parMousePos.X < Posistion.X + Size.Width &&
+             parMousePos.Y < Posistion.Y + Size.Height;
+        }
+    }
+}
---
+++ Tetris Code/TetrisLogic/Panel.cs
@@ -0,0 +1,61 @@
+﻿using System.Drawing;
+
+namespace TetrisLogic
+{
+    /// <summary>
+    /// Panel
+    /// </summary>
+    public class Panel
+    {
+        /// <summary>
+        /// Panel title
+        /// </summary>
+        private readonly string text;
+
+        /// <summary>
+        /// Panel position
+        /// </summary>
+        public PointF Posistion { set; get; }
+
+        /// <summary>
+        /// Panel size
+        /// </summary>
+        public SizeF Size { set; get; }
+
+        /// <summary>
+        /// Constructor class panel
+        /// </summary>
+        /// <param name="parText">Panel title</param>
+        public Panel(string parText)
+        {
+            text = parText;
+        }
+
+        /// <summary>
+        /// Setting the position and size of the panel
+        /// </summary>
+        /// <param name="parPos">Panel position</param>
+        /// <param name="parSize">Panel size</param>
+        public void SetBounds(PointF parPos, SizeF parSize)
+        {
+            Posistion = parPos;
+            Size = parSize;
+        }
+
+        /// <summary>
+        /// Panel update function
+        /// </summary>
+        /// <param name="graphics">Graphics</param>
+        /// <param name="parCenter">Center element or not</param>
+        public void Update(Graphics graphics, bool parCenter)
+        {
+            graphics.FillRectangle(new SolidBrush(Color.FromArgb(180, 0, 0, 0)), Posistion.X, Posistion.Y, Size.Width, Size.Height);
+            graphics.DrawRectangle(new Pen(Color.White, 2), Posistion.X, Posistion.Y, Size.Width, Size.Height);
+            graphics.DrawString(text, new Font(FontFamily.GenericSansSerif, Size.Width / 12.0f / 2),
+             Brushes.White, new RectangleF(Posistion.X, Posistion.Y + 5, Size.Width, Size.Height),
+             new StringFormat { Alignment = StringAlignment.Center, LineAlignment = parCenter ? StringAlignment.Center : StringAlignment.Near });
+
+        }
+
+    }
+}
