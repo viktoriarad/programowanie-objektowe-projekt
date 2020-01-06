12c7065167253f39f23c14d09915a5bdecca5652
Malgorzata Stepien <mrowka970@gmail.com>
2020-01-06 20:17:06 +0100

    Create unit tests

---
+++ Tetris Code/TetrisTest/UnitTestTetris.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TetrisLogic;
+
+namespace TetrisTest
+{
+    [TestClass]
+    public class UnitTestTetris
+    {
+        [TestMethod]
+        // Test the method of creating a tetris shape
+        public void TestMethodPieceCreate()
+        {
+            Piece.Create();
+            float expectedSpeed = 0.015f;
+
+            float actualSpeed = Piece.Speed;
+            PointF actualPosition = Piece.Position;
+
+            Assert.AreEqual(expectedSpeed, actualSpeed);
+        }
+
+        [TestMethod]
+        // Tetris playing field reset method test
+        public void TestMethodGameFieldReset()
+        {
+            Piece.Create();
+
+            bool expected = true;
+            GameField.Reset();
+
+            bool actual = GameField.Game;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
